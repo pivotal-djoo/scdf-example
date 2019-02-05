43b3e0e666810872efc65bd22c51cb5a5aee27c8
David Joo <djoo@pivotal.io>
2019-02-05 18:32:51 -0500

    Added readme

--- ci/deploy-stream.sh
+++ ci/deploy-stream.sh
@@ -3,16 +3,32 @@ set -e
 
 ## Undeploy the stream
 
-response_code=$(curl --write-out "%{http_code}" --silent --output /dev/null "${dataflow_server_url}/streams/deployments/${stream_name}" -i -X DELETE)
+status=$(curl --write-out "%{http_code}" --silent --output /dev/null "${dataflow_server_url}/streams/deployments/${stream_name}" -i -X DELETE)
 
-if [ ${response_code} = "200" ]; then
+response=response.txt
 
+if [ ${status} = "200" ]; then
+
+    echo "Stream undeployed successfully"
     ## If undeploy goes well, deploy the stream again to ensure updated stream apps are deployed
-    curl "${dataflow_server_url}/streams/deployments/${stream_name}" -i -X POST
+    status=$(curl -s -w %{http_code} -o ${response} "${dataflow_server_url}/streams/deployments/${stream_name}" -i -X POST)
 
+    if [ ${status} -lt 300 ]; then
+        cat ${response}
+    else
+        echo "Failed to deploy stream"
+        return -1
+    fi
 else
 
     ## If the stream doesn't exist, create and deploy it
-    curl "${dataflow_server_url}/streams/definitions" -i -X POST -d "name=${stream_name}&definition=${stream_dsl}&deploy=true"
+    echo "Stream does not exist"
+    status=$(curl -s -w %{http_code} -o ${response} "${dataflow_server_url}/streams/definitions" -i -X POST -d "name=${stream_name}&definition=${stream_dsl}&deploy=true")
 
+    if [ ${status} -lt 300 ]; then
+        cat ${response}
+    else
+        echo "Failed to create a new stream definition"
+        return -1
+    fi
 fi
